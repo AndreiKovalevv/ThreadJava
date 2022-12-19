3bc08f660166188cfa45694d7877002db36315f6
AndreiKovalevv <kovaliv.andrey007@mail.ru>
2022-12-19 18:55:35 +0400

    Потоки

---
+++ mavenproject4/src/main/java/ThreadServer.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+ 
+/**
+* Класс - сервер, принимает запросы от клиентов и отдает данные
+*/
+public class ThreadServer {
+    public static void main(String args[]) {
+        // Определяем номер порта, который будет "слушать" сервер
+        int port = 1777;
+ 
+        try {
+            // Открыть серверный сокет (ServerSocket)
+            ServerSocket servSocket = new ServerSocket(port);
+ 
+            while (true) {
+                System.out.println("Waiting for a connection on " + port);
+ 
+                // Получив соединение начинаем работать с сокетом
+                Socket fromClientSocket = servSocket.accept();
+ 
+                // Стартуем новый поток для обработки запроса клиента
+                new SocketThread(fromClientSocket).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
+
